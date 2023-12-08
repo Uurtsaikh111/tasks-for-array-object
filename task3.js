d083d4d291e07b828617482bfe96eb56ffe1cf56
Uurtsaikh111 <uuree.i4uuree@gmail.com>
2023-12-08 08:50:16 +0800

    yes

---
+++ task3.js
@@ -0,0 +1,13 @@
+function findResult(a) {
+  for (let i = a; i >= 0; i = i - 3) {
+    console.log(i);
+  }
+}
+findResult(10);
+
+function findAnswer(a, b) {
+  for (let i = b; i <= a; i = i + 1) {
+    console.log(i);
+  }
+}
+findAnswer(15, 9);
